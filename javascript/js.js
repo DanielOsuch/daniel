cbd6ae013fe68565541f9106aa73f068a2d136df
OsuchDaniel <danielekosuch@gmail.com>
2017-11-21 14:33:28 +0100

    first

---
+++ javascript/js.js
@@ -0,0 +1,10 @@
+<script>
+function scrollWin() {
+    window.scrollTo(500, 0);
+}
+</script>
+$(window).scroll(function(){
+        $('element').animate(function(){
+                //kod przesuwaj¹cy w css 
+        });
+});
